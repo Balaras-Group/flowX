c0841d7620e437aca2777f9368777ea88c94ded0
isaacfinberg <isaacfinberg@gwu.edu>
2020-03-23 20:33:57 +0000

    gauss_seidel

--- flowx/poisson/gauss_seidel.py
+++ flowx/poisson/gauss_seidel.py
@@ -1,4 +1,4 @@
-"""Routine to solve the Poisson system with Jacobi."""
+"""Routine to solve the Poisson system with Gauss-Seidel."""
 
 import numpy
 
@@ -32,3 +32,26 @@ def solve_gauss_seidel(grid, ivar, rvar, maxiter=3000, tol=1e-9, verbose=False):
         default: False.
 
     """
+    phi = grid.get_values( ivar )
+    nx, ny = grid.nx, grid.ny
+    
+    ites = 0
+    residual = tol + 1.0
+    while ites < maxiter and residual > tol:
+        phi_old = phi.copy()
+        for j in range( 1, ny - 1 ):
+            for i in range( 1, nx - 1 ):
+                phi[ j, i ] = 0.25 * ( phi_old[ j, i - 1 ] + phi_old[ j, i + 1 ] + phi_old[ j - 1, i ] + phi_old[ j + 1, i ] )
+
+        grid.fill_guard_cells( ivar )
+        residual = ( numpy.sqrt( numpy.sum( ( phi - phi_old )**2 ) / ( ( grid.nx + 2 ) * ( grid.ny + 2 ) ) ) )
+        ites += 1
+    
+    if verbose:
+        print( 'Gauss-Seidel method:' )
+            if ites == maxiter:
+                print( 'Warning: maximum number of iterations reached!' )
+            print( '- Number of iterations: {}'.format( ites ) )
+            print( '- Final residual: {}'.format( residual ) )
+
+    return ites, residual
