74ecf1a207f89cf6f79caaa7b3bcc8afa9543633
aniktarafder <aniktarafder@gwu.edu>
2020-03-29 01:14:18 +0000

    Done with SOR

--- flowx/poisson/gsor.py
+++ flowx/poisson/gsor.py
@@ -3,7 +3,7 @@
 import numpy
 
 
-def solve_gsor(grid, ivar, rvar, maxiter=3000, tol=1e-9, verbose=False):
+def solve_gsor(grid, ivar, rvar, maxiter=3000, tol=1e-9, verbose=True):
     """Solve the Poisson system using a Jacobi method.
 
     Arguments
@@ -32,3 +32,37 @@ def solve_gsor(grid, ivar, rvar, maxiter=3000, tol=1e-9, verbose=False):
         default: False.
 
     """
+    phi = grid.get_values(ivar)
+    b = grid.get_values(rvar)
+    dx, dy = grid.dx, grid.dy
+    nx, ny = grid.nx, grid.ny
+    w=1.5
+
+    ites = 0
+    residual = tol + 1.0
+    while ites < maxiter and residual > tol:
+        phi_old = numpy.copy(phi)  # previous solution
+        for j in range(1, ny - 1):
+            for i in range(1, nx - 1):
+        
+                phi[j,i] = (w*(((phi[j, i-1] +
+                          phi[j, i+1]) * dy**2 +
+                         (phi[j-1, i] +
+                          phi[j+1,i]) * dx**2 -
+                          b[j, i] * dx**2 * dy**2) /
+                           (2 * (dx**2 + dy**2)))+(1-w)*phi_old[j,i])
+
+        grid.fill_guard_cells(ivar)
+
+        residual = (numpy.sqrt(numpy.sum((phi - phi_old)**2) /
+                    ((grid.nx + 2) * (grid.ny + 2))))
+        ites += 1
+
+    if verbose:
+        print('SOR:')
+        if ites == maxiter:
+            print('Warning: maximum number of iterations reached!')
+        print('- Number of iterations: {}'.format(ites))
+        print('- Final residual: {}'.format(residual))
+
+    return ites, residual
